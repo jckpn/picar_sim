319e6654293917b71ec3d62f9c6f59fa1435d25e
DoctorDinosaur <26852749+DoctorDinosaur@users.noreply.github.com>
2024-04-29 20:21:41 +0100

    Add masking

--- utils/view_to_state.py
+++ utils/view_to_state.py
@@ -9,21 +9,54 @@ now = time.time()
 
 
 def view_to_state(img, grid_size=25):
-    # preview(img)
+    preview(img)
 
     img = overhead_warp(img)
-    # preview(img)
-    # preview(img)
+    preview(img)
 
     # img = enhance_track(img)
     # preview(img)
 
+    # Increase contrast
+    mask = cv2.convertScaleAbs(img, alpha=1.5, beta=0)
+    # preview(mask)
+
     # Convert to HSV
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
+    # preview(mask)
+
+    # Construct mask first
+    # Threshold for white pixels
+    S_MAX = 0.25
+    V_MIN = 0.75
+    mask = cv2.inRange(mask, (0, 0, int(V_MIN * 255)), (180, int(S_MAX * 255), 255))
+    # preview(mask)
+
+    # Close the mask
+    kernel = np.ones((9, 9), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    preview(mask)
+
+    # Select largest connected component
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = np.zeros_like(mask)
+    if contours:
+        max_contour = max(contours, key=cv2.contourArea)
+        cv2.drawContours(mask, [max_contour], -1, (255), -1)
+
+    preview(mask)
+
+    # Apply mask to original image, make unmasked areas red
+    img[mask == 0] = [0, 0, 255]
+
+    preview(img)
+    cv2.imwrite("utils/track.jpg", img)
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Threshold by HSV: S <= 0.3, V <= 0.5
-    S_THRESHOLD = 0.2
-    V_THRESHOLD = 0.5
+    S_THRESHOLD = 0.5
+    V_THRESHOLD = 0.25
     img = cv2.inRange(img, (0, 0, 0), (255, S_THRESHOLD * 255, V_THRESHOLD * 255))
     img = cv2.dilate(img, np.ones((5, 5), np.uint8), iterations=1)
     img = cv2.resize(img, (grid_size, grid_size), interpolation=cv2.INTER_NEAREST)
@@ -31,7 +64,6 @@ def view_to_state(img, grid_size=25):
     preview(img)
 
     state = img / 255
-    # Plot a histogram of the state
     print(state.shape)
     return state
 
@@ -39,10 +71,10 @@ def view_to_state(img, grid_size=25):
 def preview(img):
     global now
     print(f"time: {(time.time() - now)*1000:.2f}ms")
-    preview_img = cv2.resize(img, (320, 240), interpolation=cv2.INTER_NEAREST)
-    cv2.imshow("", preview_img)
-    cv2.waitKey(0)
     now = time.time()
+    preview_img = cv2.resize(img, (320, 240), interpolation=cv2.INTER_NEAREST)
+    cv2.imshow(str(now), preview_img)
+    # cv2.waitKey(0)
 
 
 def overhead_warp(img):
@@ -98,16 +130,21 @@ def enhance_track(img):
 def print_state(state):
     """Print the state as a 25x25 grid of characters. ' ' for < 0.5, '█' for >= 0.5"""
     for row in state:
-        print("".join(["░" if cell < 0.5 else "█" for cell in row]))
+        print("".join(["░" if cell < 0.75 else "█" for cell in row]))
 
 
 if __name__ == "__main__":
     img = cv2.imread(
-        "C:/Users/Dino/Downloads/New folder1/Car1/downloads_images/1709571067912_90_35.png"
+        f"C:/Users/Dino/OneDrive - The University of Nottingham/Year 4 MSc/MLiS P2/notts-mlis-picar/data/training_data/training_data/{np.random.randint(1, 10000)}.png"
     )
+    # img = cv2.imread(
+    #    r"C:\Users\Dino\Downloads\New folder1\Car1\downloads_images\1709571067912_90_35.png"
+    # )
     # img = cv2.imread("utils/pic.jpg")
     state = view_to_state(img)
     print_state(state)
+
+    cv2.waitKey(0)
 
     exit()
 
